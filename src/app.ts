f0a0fd6af5faaf765ec3cc4003069e10c2330a0b
Jade Francis Palco <jfbpalco+dev@gmail.com>
2025-05-21 04:47:31 +0800

    add linting and pre-commit hooks

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from 'express';
+
+import routes from './routes/index';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 3000; // Always use process.env.PORT else default to 3000
-
-import express, { NextFunction, Request, Response } from 'express';
-import routes from './routes/index';
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use('/api', routes);
 
 // Error handling middleware
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
     console.error(err.stack); // Log the error stack
     res.status(500).send('Something broke!'); // Send a generic error response
 });
